005f9b1d4bd04e649507493cea62fec525326a15
MatiasBcEz <m.cid@duocuc.cl>
2022-10-16 23:06:50 -0300

    login

--- Prueba2/src/app/app-routing.module.ts
+++ Prueba2/src/app/app-routing.module.ts
@@ -12,6 +12,11 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'login',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: '**',
     redirectTo: 'e404',
     pathMatch: 'full'
@@ -20,6 +25,11 @@ const routes: Routes = [
     path: 'e404',
     loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+
 
 ];
 
