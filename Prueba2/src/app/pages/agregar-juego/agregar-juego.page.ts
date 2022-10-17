2cdacc6dd05215fb95285fac5f8332537c1a808a
MatiasBcEz <m.cid@duocuc.cl>
2022-10-17 01:44:05 -0300

    validaciones1

--- Prueba2/src/app/pages/agregar-juego/agregar-juego.page.ts
+++ Prueba2/src/app/pages/agregar-juego/agregar-juego.page.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarJuegoPage implements OnInit {
 
+  modeloNombreJuego : string = '';  
+  modeloDescripcion : string = '';  
+  modeloPrecio : string = ''; 
+  modeloLink : string = ''; 
+  modeloCategoria : string = '';  
+  modeloSKU : string = '';  
+  modeloLanzamiento : string = '';  
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  validarJuego(){
+    console.log(this.modeloNombreJuego);
+    console.log(this.modeloDescripcion);
+    console.log(this.modeloPrecio);
+    console.log(this.modeloLink);
+    console.log(this.modeloCategoria);
+    console.log(this.modeloSKU);
+    console.log(this.modeloLanzamiento);
   }
 
 }
