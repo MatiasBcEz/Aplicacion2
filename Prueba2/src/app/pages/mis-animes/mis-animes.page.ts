0d87789566cbf32f70514ab4bd2ae258d4dfdaf8
Malva <ig.orozco@duocuc.cl>
2022-10-18 02:35:36 -0300

    arreglo api

--- Prueba2/src/app/pages/mis-animes/mis-animes.page.ts
+++ Prueba2/src/app/pages/mis-animes/mis-animes.page.ts
@@ -8,16 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MisAnimesPage implements OnInit {
 
+  animes = []
   constructor(private http:HttpClient) { 
 
   }
 
   ngOnInit() {
-    this.http.get('https://animechan.vercel.app/api/random')
+    this.http.get<any>('https://rickandmortyapi.com/api/character')
     .subscribe(res => {
       console.log(res);
+      this.animes = res.results;
       
     })
   }
+  
 
 }
+
+// https://animechan.vercel.app/api/random
